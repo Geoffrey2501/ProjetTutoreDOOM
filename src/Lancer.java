1480cf57ebbc7477da559e8363b43660b1fcfc8b
Cyprian <cyprian.chailan@icloud.com>
2025-12-15 11:35:45 +0100

    classe Map.java

--- src/Lancer.java
+++ src/Lancer.java
@@ -1,7 +1,15 @@
 import com.sun.tools.javac.Main;
+import prototype_raycasting.Map;
 
 public class Lancer {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Map m = new Map("src/prototype_raycasting/map/map.txt");
+        System.out.println("W=" + m.getWIDTH() + " H=" + m.getHEIGHT());
+        for (int y = 0; y < m.getHEIGHT(); y++) {
+            for (int x = 0; x < m.getWIDTH(); x++) {
+                System.out.print(m.isWall(x, y) ? "#" : ".");
+            }
+            System.out.println();
+        }
     }
 }
