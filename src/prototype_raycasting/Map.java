24e1ddac19d2c407778848871244aaeb293df47d
Nilecram <marcelin.ray@protonmail.com>
2025-12-15 10:47:21 +0100

    Base Map

---
+++ src/prototype_raycasting/Map.java
@@ -0,0 +1,27 @@
+package prototype_raycasting;
+
+public class Map {
+    private int WIDTH;
+    private int HEIGHT;
+
+    public Map () {
+
+    }
+
+
+    public Map(String fichier){
+
+    }
+
+    public boolean[][] getMap() {
+        return null;
+    }
+
+    public int getWIDTH() {
+        return WIDTH;
+    }
+
+    public int getHEIGHT() {
+        return HEIGHT;
+    }
+}
