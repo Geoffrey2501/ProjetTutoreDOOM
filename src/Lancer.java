d96b63e57d00bfa09f78c556c33d05684470fbfa
Geoffrey2501 <132345682+Geoffrey2501@users.noreply.github.com>
2025-12-15 10:31:28 +0100

    Update Lancer.java

--- src/Lancer.java
+++ src/Lancer.java
@@ -2,6 +2,6 @@ import com.sun.tools.javac.Main;
 
 public class Lancer {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
 }
