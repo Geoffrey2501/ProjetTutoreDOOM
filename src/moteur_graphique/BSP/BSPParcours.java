2bb618907272d23d48e224dc2ed39522f199ab5c
Nilecram <marcelin.ray@protonmail.com>
2026-02-17 14:29:18 +0100

    ajout de la methode pour avoir les murs visibles

--- src/moteur_graphique/BSP/BSPParcours.java
+++ src/moteur_graphique/BSP/BSPParcours.java
@@ -44,4 +44,53 @@ public class BSPParcours implements GameRenderer {
         Renderer r = new Renderer();
         r.renderFourPointsList(g, width, height, pointsToDraw);
     }
+
+    public List<Mur> getMursVisibles(double x, double y) {
+        //à 360 degres, on appelle juste 3 fois getMursVisiblesAngle avec un fov de 120, et on merge les listes en supprimant les doublons
+        //180 de fov et 360 ne fonctionnent pas. 120x3 = 360 donc on peut scan tous les murs visibles
+        List<Mur> mursVisibles = new ArrayList<>();
+        mursVisibles.addAll(getMursVisiblesAngle(x, y, 0, 120));
+        mursVisibles.addAll(getMursVisiblesAngle(x, y, 120, 120));
+        mursVisibles.addAll(getMursVisiblesAngle(x, y, 240, 120));
+
+        //supprimer les doublons
+        List<Mur> mursVisiblesSansDoublons = new ArrayList<>();
+        for (Mur m : mursVisibles) {
+            if (!mursVisiblesSansDoublons.contains(m)) {
+                mursVisiblesSansDoublons.add(m);
+            }
+        }
+
+        return mursVisiblesSansDoublons;
+    }
+
+    private List<Mur> getMursVisiblesAngle(double x, double y, double angle, int fov) {
+        //on fait un algo similaire à render, juste on n'a pas besoin de la reel taille d'écran.
+        //on peut aussi avoir 1 de fov par exemple juste pour avoir le mur en face et limiter les calculs
+        int width = 720;
+        int height = 1080;
+
+        FrontToBack frontToBack = new FrontToBack(this.arbreBSP, x, y);
+        FilledScreen filledScreen = new FilledScreen(width);
+        WallCalcul wallCalcul = new WallCalcul();
+
+        List<Mur> mursVisibles = new ArrayList<>();
+
+        Mur m = frontToBack.getNextWall();
+        while(m != null && !filledScreen.isFull()) {
+            FourPoints points = wallCalcul.getFourPoints(m, x, y, fov, angle, width, height);
+            if (points != null) {
+                List<FourPoints> pointsAfterFilledScreen = filledScreen.add(points);
+                if (pointsAfterFilledScreen != null) {
+                    mursVisibles.add(m);
+                    //moins optimisé, car on peut avoir des murs partiellement visibles
+                    //mais en soit plus simple pour les calculs de collisions au final ?
+                    //à voir
+                }
+            }
+            m = frontToBack.getNextWall();
+        }
+
+        return mursVisibles;
+    }
 }
--- src/moteur_graphique/BSP/CollisionBSP.java
+++ src/moteur_graphique/BSP/CollisionBSP.java
@@ -6,14 +6,17 @@ import java.awt.geom.Line2D;
 
 public class CollisionBSP implements CollisionStrategy {
     private final MapMur map;
+    BSPParcours bspParcours;
 
     public CollisionBSP(MapMur map){
         this.map = map;
+        this.bspParcours = new BSPParcours(null, map);
     }
 
     @Override
     public boolean isColliding(double x, double y, double radius) {
-        for (Mur mur : map.getMurs()) {
+        //for (Mur mur : map.getMurs()) {
+        for (Mur mur : this.bspParcours.getMursVisibles(x, y)) {
             double distance = Line2D.ptSegDist(mur.x0, mur.y0, mur.x1, mur.y1, x, y);
 
             if (distance < radius) {
